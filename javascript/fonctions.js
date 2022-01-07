c18ff0c238e14032ce853e529e3c91f1cf639b1b
59antoineC <97181345+59antoineC@users.noreply.github.com>
2022-01-07 10:50:32 +0100

    Add files via upload

--- javascript/fonctions.js
+++ javascript/fonctions.js
@@ -1,5 +1,9 @@
 function init()	{let  inner = document.getElementById("inner");
 				inner.setAttribute("class","javascript");
+				let body = document.getElementById("moncv");
+				body.setAttribute("class","light");
+				let header = document.getElementById("header");
+				header.setAttribute("onclick","screenMode();return false;");
 				let contenu = document.getElementById("contenu");
 				let a = document.createElement("a");
 				contenu.appendChild(a);
@@ -31,5 +35,13 @@ function retour()	{let inner = document.getElementById("inner");
 			  partie[i].style.display = "none";
 				}
 			inner.style.transform = "rotateY(0)";
+			return false;
+			}
+
+function screenMode()	{let elm = document.getElementById("moncv");
+			if(elm.className === 'dark') {
+   				 elm.className = 'light';
+				}
+			else {elm.className = 'dark';}
 			return false;
 			}
